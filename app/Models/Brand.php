ccab8432461ed1a38f2df95c526b974e03639511
Omar Abi Farraj <omar.7tech@gmail.com>
2026-05-29 01:01:24 +0300

    publish configs for slaggable

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Guarded;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
@@ -13,8 +12,7 @@ use Spatie\Sluggable\SlugOptions;
 
 class Brand extends Model 
 {
-    /** @use HasFactory<\Database\Factories\BrandFactory> */
-    use HasFactory, HasSlug;
+    use HasSlug;
 
     protected $casts = [
         'show_on_website' => 'boolean',
