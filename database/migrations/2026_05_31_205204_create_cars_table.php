36bd27028fb4764f5b0260cc26bba37ed9e89249
Omar Abi Farraj <omar.7tech@gmail.com>
2026-05-31 23:57:18 +0300

    make the car models and data

---
+++ database/migrations/2026_05_31_205204_create_cars_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('car_model_id')->constrained()->cascadeOnDelete();
+            $table->string('plate_code', 10);
+            $table->string('plate_number');
+            $table->unique(['plate_code', 'plate_number']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
